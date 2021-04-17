c360422ce539f6c2d2ef169e2fd7c6f098705026
IvanCo <ivanack2015@gmail.com>
2021-04-17 18:25:39 +0300

    Initial Commit

---
+++ SlideMenuStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideMenuStoryBoard
+//
+//  Created by Ivan Ivanov on 4/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
